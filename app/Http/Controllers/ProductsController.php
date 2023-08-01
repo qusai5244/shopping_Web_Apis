05a3fb40d7f8fdb560ab8badb303301f122d76f6
Qusai <qalnaabi1@sheffield.ac.uk>
2023-08-01 22:03:09 +0100

    create product model and controller

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        // how to return products as json
+        return response()->json([$products]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validate the input data
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0'
+        ]);
+
+        // Create a new product
+        $product = new Product();
+        $product->product_name = $request->name;
+        $product->product_price = $request->price;
+        $product->save();
+
+        // Return a response with a resource JSON
+        return response()->json([
+            'data' => $product,
+            'message' => 'Product created successfully!'
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Get the product
+        $product = Product::find($id);
+        // Return a response with a resource JSON
+        return response()->json([
+            'data' => $product,
+            'message' => 'Product retrieved successfully!'
+        ], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Validate the input data
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0'
+        ]);
+
+        // Get the product
+        $product = Product::find($id);
+        $product->product_name = $request->name;
+        $product->product_price = $request->price;
+        $product->save();
+
+        // Return a response with a resource JSON
+        return response()->json([
+            'data' => $product,
+            'message' => 'Product updated successfully!'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // Get the product
+        $product = Product::find($id);
+        $product->delete();
+
+        // Return a response with a resource JSON
+        return response()->json([
+            'data' => $product,
+            'message' => 'Product deleted successfully!'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\ShoppingCartController;
+use App\Http\Controllers\ProductsController;
 
 
 /*
@@ -23,6 +24,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::resource('users', UsersController::class); // User API
+Route::resource('products', ProductsController::class); // products API
 
 Route::get('users/shopping-cart/{cart_id}', [UsersController::class, 'getUserCart']); // Get user's shopping cart
 
